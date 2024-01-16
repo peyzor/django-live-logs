f5abae2a5a573799e73180afb1b938fac56c6fa2
Peyman <hassani.peyman627@gmail.com>
2024-01-16 11:07:31 +0330

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./log_viewer/templates/log_viewer/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
