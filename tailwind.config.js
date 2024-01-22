0f03309d9ecbe6deedec3b8bdb4180af2dc2830b
shaghayegh <valadkhani.sh@gmail.com>
2024-01-22 17:22:14 +0330

    add dark mode button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
+
     content: ["./log_viewer/templates/log_viewer/*.html"],
     theme: {
         extend: {},
